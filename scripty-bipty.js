240dbc34b05d31e35050a4517448cc448b9c3e43
KalDrongo <65057112+KalDrongo@users.noreply.github.com>
2023-04-26 12:52:48 -0700

    Update scripty-bipty.js

--- scripty-bipty.js
+++ scripty-bipty.js
@@ -7,20 +7,20 @@ canvas.height = window.innerHeight;
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }           
-var canvas = document.getElementById("starfield"),
-context = canvas.getContext("2d"),
-stars = 500,
-colorrange = [0,60,240];
-for (var i = 0; i < stars; i++) {
-var x = Math.random() * canvas.offsetWidth;
-y = Math.random() * canvas.offsetHeight,
-radius = Math.random() * 1.2,
-hue = colorrange[getRandom(0,colorrange.length - 1)],
-sat = getRandom(50,100);
-context.beginPath();
-context.arc(x, y, radius, 0, 360);
-context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
-context.fill();
+    var canvas = document.getElementById("starfield"),
+    context = canvas.getContext("2d"),
+    stars = 500,
+    colorrange = [0,60,240];
+    for (var i = 0; i < stars; i++) {
+    var x = Math.random() * canvas.offsetWidth;
+    y = Math.random() * canvas.offsetHeight,
+    radius = Math.random() * 1.2,
+    hue = colorrange[getRandom(0,colorrange.length - 1)],
+    sat = getRandom(50,100);
+    context.beginPath();
+    context.arc(x, y, radius, 0, 360);
+    context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
+    context.fill();
 }
 
 //keeps track of frame number for timing
